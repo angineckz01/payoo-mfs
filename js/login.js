a9a2c0240c707fb5d5d3e62c2d10a01128f94c0f
Your Name <angineckz01@gmail.com>
2025-03-16 09:00:30 +0600

    added home external file if user could log in there

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('login-button').addEventListener('click', function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+    if(phoneNumber === '01' && pinNumber ==="1234"){
+        console.log('You are logged in.')
+        window.location.href="/home.html"
+    }
+    else{
+        alert('wrong phone number or pin');
+    }
+})
